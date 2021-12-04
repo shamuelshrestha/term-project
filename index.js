2cb25d4a5d75554ef64440a8e554162bd3c8cb29
Shamuel Shrestha <changingmyself.abyssopelagic@gmail.com>
2021-12-04 20:28:04 +0545

    testing partials in hbs

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const path = require('path')
+const hbs = require('hbs')
 
 const app = express()
 
 // Handlebars setup
 app.set('view engine', 'hbs')
 
+// setting partials 
+hbs.registerPartials(__dirname + '/views/partials')
 
 // routes handlers
 app.use('/', require('./routes/index'))
